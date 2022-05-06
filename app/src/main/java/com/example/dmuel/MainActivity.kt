e759e7dbae99c6d763b537b2e1de034461bfa80b
wlsdn9297 <wlsdn9297@naver.com>
2022-05-06 13:42:31 +0900

    시작화면, 로그인 페이지

--- app/src/main/java/com/example/dmuel/MainActivity.kt
+++ app/src/main/java/com/example/dmuel/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.dmuel
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dmuel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            var intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
