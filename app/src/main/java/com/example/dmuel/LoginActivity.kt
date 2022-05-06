43d8048f582976e46ac7e693b5dbc0fc4fc302c4
wlsdn9297 <wlsdn9297@naver.com>
2022-05-06 16:59:44 +0900

    회원가입 페이지

--- app/src/main/java/com/example/dmuel/LoginActivity.kt
+++ app/src/main/java/com/example/dmuel/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.dmuel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dmuel.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            //var intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinButton.setOnClickListener {
+            var intent = Intent(applicationContext, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
