ab265d07d02605095c1fa8e582f15e3fc0088b54
jeonchaerin <chaerin0411@naver.com>
2022-05-20 12:55:33 +0900

    커뮤니티 화면 레이아웃

--- app/src/main/java/com/example/dmuel/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/dmuel/CommunityDetailActivity.kt
@@ -3,111 +3,14 @@ package com.example.dmuel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.dmuel.databinding.ActivityCommunityDetailBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.example.dmuel.model.ContentDTO
-import com.example.dmuel.model.AlarmDTO
-import com.example.dmuel.util.FcmPush
-import com.google.android.gms.fido.fido2.api.common.RequestOptions
 
 class CommunityDetailActivity : AppCompatActivity() {
     val binding by lazy { ActivityCommunityDetailBinding.inflate(layoutInflater) }
-    var contentUid: String? = null
-    var destinationUid: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        contentUid = intent.getStringExtra("contentUid")
-        destinationUid = intent.getStringExtra("destinationUid")
-
-        binding.commentRecyclerview.adapter = CommentRecyclerviewAdapter()
-        binding.commentRecyclerview.layoutManager = LinearLayoutManager(this)
-
-        binding.commentSendBtn?.setOnClickListener {
-            var comment = ContentDTO.Comment()
-            comment.userId = FirebaseAuth.getInstance().currentUser?.email
-            comment.uid = FirebaseAuth.getInstance().currentUser?.uid
-            comment.comment = binding.commentMessageEdit.text.toString()
-            comment.timestamp = System.currentTimeMillis()
-
-            FirebaseFirestore.getInstance().collection("images").document(contentUid!!)
-                .collection("comments").document().set(comment)
-            commentAlarm(destinationUid!!, binding.commentMessageEdit.text.toString())
-            binding.commentMessageEdit.setText("")
-        }
-    }
-
-    fun commentAlarm(destinationUid: String, message: String) {
-        var alarmDTO = AlarmDTO()
-        alarmDTO.destinationUid = destinationUid
-        alarmDTO.userId = FirebaseAuth.getInstance().currentUser?.email
-        alarmDTO.kind = 1
-        alarmDTO.uid = FirebaseAuth.getInstance().currentUser?.uid
-        alarmDTO.timestamp = System.currentTimeMillis()
-        alarmDTO.message = message
-        FirebaseFirestore.getInstance().collection("alarms").document().set(alarmDTO)
-
-        var msg = FirebaseAuth.getInstance().currentUser?.email + " left a message of " + message
-        FcmPush.instance.sendMessage(destinationUid, "community", msg)
-    }
-
-    inner class CommentRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-        var comments: ArrayList<ContentDTO.Comment> = arrayListOf()
-
-        init {
-            FirebaseFirestore.getInstance()
-                .collection("images")
-                .document(contentUid!!)
-                .collection("comments")
-                .orderBy("timestamp")
-                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                    comments.clear()
-                    if (querySnapshot == null) return@addSnapshotListener
-
-                    for (snapshot in querySnapshot.documents!!) {
-                        comments.add(snapshot.toObject(ContentDTO.Comment::class.java)!!)
-                    }
-                    notifyDataSetChanged()
-                }
-        }
-
-        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-            var view =
-                LayoutInflater.from(p0.context).inflate(R.layout.comment_item_view, p0, false)
-            return CustomViewHolder(view)
-        }
-
-        private inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-        override fun getItemCount(): Int {
-            return comments.size
-        }
-
-        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-//            var view = p0.itemView
-//            view.commentExplain.text = comments[p1].comment
-//            view.profileImage.text = comments[p1].userId
-//
-//            FirebaseFirestore.getInstance()
-//                .collection("profileImages")
-//                .document(comments[p1].uid!!)
-//                .get()
-//                .addOnCompleteListener { task ->
-//                    if(task.isSuccessful){
-//                        var url = task.result!!["image"]
-//                        Glide.with(p0.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentviewitem_imageview_profile)
-//                    }
-//                }
-//        }
-        }
     }
 }
