ac0cf81c7b02ba72158a48ada97e187ffe9848cc
jeonchaerin <chaerin0411@naver.com>
2022-06-02 04:57:29 +0900

    운동 가이드, 커뮤니티 화면 수정

--- app/src/main/java/com/example/dmuel/CommunityCommentActivity.kt
+++ app/src/main/java/com/example/dmuel/CommunityCommentActivity.kt
@@ -3,10 +3,10 @@ package com.example.dmuel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dmuel.databinding.ActivityCommunityDetailBinding
+import com.example.dmuel.databinding.ActivityCommunityCommentBinding
 
-class CommunityDetailActivity : AppCompatActivity() {
-    val binding by lazy { ActivityCommunityDetailBinding.inflate(layoutInflater) }
+class CommunityCommentActivity : AppCompatActivity() {
+    val binding by lazy { ActivityCommunityCommentBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/dmuel/WorkoutGuidActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dmuel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dmuel.databinding.ActivityWorkoutGuidBinding
+
+class WorkoutGuidActivity : AppCompatActivity() {
+    val binding by lazy { ActivityWorkoutGuidBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
